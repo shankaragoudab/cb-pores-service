e59fa223bd18e392624e01373731660fcf0d9dcc
Arpitha <arpitha.sureshappa@tarento.com>
2025-07-04 17:48:05 +0530

    announcement range issue fix (#95)
    * demand search API issue fix
    
    * demand search API issue fix
    
    * announcement range issue fixed
    
    * announcement range issue fix

--- src/main/java/com/igot/cb/pores/elasticsearch/service/EsUtilServiceImpl.java
+++ src/main/java/com/igot/cb/pores/elasticsearch/service/EsUtilServiceImpl.java
@@ -270,16 +270,16 @@ public class EsUtilServiceImpl implements EsUtilService {
                                 nestedMap.forEach((rangeOperator, rangeValue) -> {
                                     switch (rangeOperator) {
                                         case Constants.SEARCH_OPERATION_GREATER_THAN_EQUALS:
-                                            rangeQuery.gte((JsonData) rangeValue);
+                                            rangeQuery.gte(JsonData.of(rangeValue));
                                             break;
                                         case Constants.SEARCH_OPERATION_LESS_THAN_EQUALS:
-                                            rangeQuery.lte((JsonData) rangeValue);
+                                            rangeQuery.lte(JsonData.of( rangeValue));
                                             break;
                                         case Constants.SEARCH_OPERATION_GREATER_THAN:
-                                            rangeQuery.gt((JsonData) rangeValue);
+                                            rangeQuery.gt(JsonData.of( rangeValue));
                                             break;
                                         case Constants.SEARCH_OPERATION_LESS_THAN:
-                                            rangeQuery.lt((JsonData) rangeValue);
+                                            rangeQuery.lt(JsonData.of( rangeValue));
                                             break;
                                     }
                                 });

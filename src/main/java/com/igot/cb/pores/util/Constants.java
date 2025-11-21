a88d04399d69f00bce4865b39f72ef823a4eb695
Arpitha <arpitha.sureshappa@tarento.com>
2025-11-21 16:49:25 +0530

    KB-11974-case sensitive Sort issue fix for contentpartner name (#118)
    * KB-12079-Sort enhancement for non text fields
    
    * KB-11974-case sensitive Sort issue fix for contentpartner name

--- src/main/java/com/igot/cb/pores/util/Constants.java
+++ src/main/java/com/igot/cb/pores/util/Constants.java
@@ -311,7 +311,7 @@ public class Constants {
     public static final String FRAMEWORK = "framework";
     public static final String CHANNELS = "channels";
     public static final String MASTER = "master";
-    public static final String CONTENT_PROVIDER_INDEX_NAME = "content_provider";
+    public static final String CONTENT_PROVIDER_INDEX_NAME = "content_provider_alias";
     public static final String CONTENT_PARTNER = "contentPartner";
     public static final String CATEGORIES  = "categories";
     public static final String ASSOCIATIONS = "associations";

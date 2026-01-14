738c3deec4945070c9937befd9dc4ff8979ce0f2
arpithasureshappa <arpitha.sureshappa@tarento.com>
2026-01-15 00:53:16 +0530

    KB-12658 fix to fetch active contents

--- src/main/java/com/igot/cb/cios/service/impl/CiosContentServiceImpl.java
+++ src/main/java/com/igot/cb/cios/service/impl/CiosContentServiceImpl.java
@@ -395,6 +395,8 @@ public class CiosContentServiceImpl implements CiosContentService {
                 }
                 searchCriteria.setFilterCriteriaMap(filterCriteriaMap);
             }
+            HashMap<String, Object> filterCriteriaMap = searchCriteria.getFilterCriteriaMap();
+            filterCriteriaMap.put(Constants.IS_ACTIVE, true);
             SearchResult searchResult = redisTemplate.opsForValue()
                     .get(generateRedisJwtTokenKey(searchCriteria));
             if (searchResult != null) {
